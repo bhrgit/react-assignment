b8fe62585b596d45a7b8cee5fe07752d43874476
bhrgit <bhr.shipaichi@gmail.com>
2026-09-13 22:38:38 +0600

    btn click

--- src/Components/ProgData.jsx
+++ src/Components/ProgData.jsx
@@ -4,6 +4,11 @@ import React, { use } from 'react';
 const ProgData = ({ progDataPromise }) => {
         const tech = use(progDataPromise);
         console.log('Fetched programming data:', tech); // Debugging line to check the fetched data
+        const [isSelected, setIsSelected] = React.useState(false);
+        const handleSelectStack = () => {
+        setIsSelected(!isSelected)
+        };
+
         return (
             <div>
             {/* <Catalog tech={tech} /> */}
@@ -18,8 +23,8 @@ const ProgData = ({ progDataPromise }) => {
                         <div ClassName="w-full flex flex-row gap-4 m-4 p-4 bg-gray-100 rounded shadow">                        
 
                         
-                <div className="w-3/4 grid grid-cols-3  gap-4 m-4 p-4 bg-gray-100 rounded shadow">
-                    <div>
+                <div >
+                    <div className="w-3/4 grid grid-cols-3  gap-4 m-1 p-1 bg-gray-100 rounded shadow">
                         <div key={t.id} className="bg-white p-4 rounded shadow">
                             <div className="grid grid-cols-2 gap-2 mt-2">
                                 <img src={t.icon} alt={t.name} className="w-8 h-8 " />
@@ -30,15 +35,22 @@ const ProgData = ({ progDataPromise }) => {
                             <h3 className="text-lg font-semibold text-gray-800">{t.name}</h3>
                             <p className="text-gray-600">{t.description}</p>
                             
-                            <div className="grid grid-cols-3 gap-2 mt-2">
-                                <span className="badge badge-xs badge-warning">{t.category}</span>
-                                <span className="badge badge-xs badge-warning">{t.difficulty}</span>
-                                <span className="badge badge-xs badge-warning">{t.rating}</span>
+                            <div className="grid grid-cols-3 align-items space-between gap-1 mt-2">
+                                <span className="badge badge-md badge-warning text-balance">{t.category}</span>
+                                <span className="badge badge-md badge-warning text-balance">{t.difficulty}</span>
+                                <span className="badge badge-xs badge-soft">{t.rating}</span>
                             </div>
 
                             <div>
+                                
+                                    
+                                
                                 <div className="mt-2">
-                                <button className="btn btn-sm btn-block bg-black text-white">Add to Stack</button>
+                                <button onClick={() => handleSelectStack()} 
+                                className="btn btn-sm btn-block bg-black text-white transition-all hover:scale-105">
+                                {isSelected === true ? "Added" : "Add to Stack"}
+                                </button>
+                            
                                 </div>
                             </div>
                         </div>                           
